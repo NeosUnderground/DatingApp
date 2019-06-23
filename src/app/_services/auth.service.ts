017beba45d7a08e15bcee80edc85d7d9731aff6a
NeosUnderground <33915360+NeosUnderground@users.noreply.github.com>
2019-06-23 15:44:57 -0500

    added nav functionality and bootswatch styling

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {map} from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
+import {JwtHelperService} from '@auth0/angular-jwt';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,8 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
 
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
 constructor(private http: HttpClient) { }
 
@@ -17,6 +21,7 @@ constructor(private http: HttpClient) { }
           const user = response;
             if (user) {
               localStorage.setItem('token', user.token);
+              this.decodedToken = this.jwtHelper.decodeToken(user.token);
             }
         })
       );
@@ -24,5 +29,10 @@ constructor(private http: HttpClient) { }
 
   register(model: any){
     return this.http.post(this.baseUrl + 'register', model);
+  }
+
+  logginIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
